d0e7483a2c1944c554860c63513a6c735a6aed9a
navaneethkour02 <navneethproject02@gmail.com>
2017-04-16 03:18:52 -0400

    added code to get ami,az details on eureka

--- src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
+++ src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
@@ -3,6 +3,11 @@ package com.virtualpairprogrammers;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.commons.util.InetUtils;
+import org.springframework.cloud.netflix.eureka.EurekaInstanceConfigBean;
+import org.springframework.context.annotation.Bean;
+
+import com.netflix.appinfo.AmazonInfo;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,5 +15,18 @@ public class PositionreceiverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PositionreceiverApplication.class, args);
+	}
+	
+	@Bean
+	public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
+	{
+	   EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
+	   AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
+	   instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
+	   instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
+	   instance.setDataCenterInfo(info);
+	   instance.setNonSecurePort(8080);
+	   return instance;   
+	   
 	}
 }
